d6d4ceabf086dcae499af989304a6d896640fe8a
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2020-11-26 16:21:46 +0800

    New

---
+++ app/src/main/java/com/example/express_eat/User.java
@@ -0,0 +1,17 @@
+package com.example.express_eat;
+
+public class User {
+
+    public String username, email, location;
+
+    public User(){
+
+    }
+
+    public User(String UserName, String email, String location){
+        this.username = UserName;
+        this.email = email;
+        this.location = location;
+
+    }
+}
